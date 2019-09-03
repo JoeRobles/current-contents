72fef8fe10e07875bec5cd8a63a05b203082166a
Joe Robles <joe.robles.pdj@gmail.com>
2019-09-03 00:50:52 -0500

    feature/validations for create author implemented

--- handler.js
+++ handler.js
@@ -1,32 +1,50 @@
 'use strict';
 const AWS = require('aws-sdk');
-const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10'});
-const postTable = process.env.POST_TABLE;
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+const postsTable = process.env.POSTS_TABLE;
 const uniqid = require('uniqid');
 
 const response = (statusCode, message) => {
   return {
     statusCode,
     body: JSON.stringify(message)
-  }
+  };
 };
 
-module.exports.createAuthor = async (event, context, callback) => {
+module.exports.createAuthor = (event, context, callback) => {
   const reqBody = JSON.parse(event.body);
+
+  if (
+    !reqBody.authorName ||
+    reqBody.authorName.trim() === '' ||
+    !reqBody.birthDate ||
+    reqBody.birthDate.trim() === '' ||
+    !reqBody.email ||
+    reqBody.email.trim() === ''
+  ) {
+    return callback(
+      null,
+      response(400, {
+        error: 'Post must have a title and body and they must not be empty'
+      })
+    );
+  }
+
   const post = {
     authorId: uniqid(),
     authorName: reqBody.authorName,
     birthDate: reqBody.birthDate,
     email: reqBody.email,
-    createdAt: newDate().toISOString(),
+    createdAt: new Date().toISOString(),
   };
 
   return db.put({
-    TableName: postTable,
+    TableName: postsTable,
     Item: post
   })
-  .promise().then(() => {
-    callback(null, response(201, post))
+  .promise()
+  .then(() => {
+      callback(null, response(201, post));
   })
   .catch(err => response(null, response(err.statusCode, err)));
 };
